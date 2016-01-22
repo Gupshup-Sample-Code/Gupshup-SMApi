73121a8fa4f8f1beebebb6f84026699a681a601c
Kauntal-Patel <kauntal.patel@webaroo.com>
2016-01-22 17:37:34 +0530

    Create Handler.java

---
+++ Customer-support-feedback-via-SMS/Handler.java
@@ -0,0 +1,60 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Handler
+ */
+@WebServlet("/handler")
+public class Handler extends HttpServlet
+{
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException
+	{
+		PrintWriter writer = response.getWriter();
+
+		try
+		{
+			String[] mobileArray = null;
+			String apiKey = "YOUR API KEY";// TODO:
+			SMAPIOperatios smApi = new SMAPIOperatios(apiKey);
+			String mobile = req.getParameter("mobile");
+			String message = req.getParameter("message");
+			int pollId = Integer.parseInt(req.getParameter("chatletid"));
+			if(mobile.contains(","))
+			{
+				mobileArray = mobile.split(",");
+				for(int i=0;i<mobileArray.length;i++)
+				{
+					String sLink = smApi.generateSignedLink(pollId, mobileArray[i]);
+					smApi.sendSMS(mobileArray[i],message,sLink);
+				}
+			}else
+			{
+				String sLink = smApi.generateSignedLink(pollId, mobile);
+				smApi.sendSMS(mobile,message,sLink);
+			}
+			writer.write("Success.");
+		} catch (Exception e)
+		{
+			writer.write(""+e);
+		}
+
+		finally
+		{
+			if (writer != null)
+			{
+				writer.flush();
+				writer.close();
+			}
+		}
+
+	}
+
+}
