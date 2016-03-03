5292edf09277d8b3933680c29ac486f6167d343f
gupshup.io <kauntal.patel@webaroo.com>
2016-03-03 12:39:07 +0530

    Create Handler.java

---
+++ Public-voting-for-movie-awards-via-SMS/Handler.java
@@ -0,0 +1,51 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Handler
+ */
+@WebServlet("/handler")
+public class Handler extends HttpServlet
+{
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException
+	{
+		PrintWriter writer = response.getWriter();
+
+		try
+		{
+			String[] mobileArray = null;
+			String apiKey = "YOUR API KEY";// TODO:
+			SMAPIOperatios smApi = new SMAPIOperatios(apiKey);
+			String mobile = req.getParameter("mobile");
+			String message = req.getParameter("message");
+			int formId = Integer.parseInt(req.getParameter("chatletid")); create a Survey using gupshup.io swagger
+			
+			String sLink = smApi.generateSignedLink(formId, mobile);
+			smApi.sendSMS(mobileArray[i],message,sLink);
+			writer.write("Success.");
+			
+		} catch (Exception e)
+		{
+			writer.write(""+e);
+		}
+
+		finally
+		{
+			if (writer != null)
+			{
+				writer.flush();
+				writer.close();
+			}
+		}
+
+	}
+
+}
